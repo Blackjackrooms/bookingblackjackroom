332292533d428a0bd11fadc26e464cc2cd3ffd09
Blackjackrooms <mhmmddimas075@gmail.com>
2026-05-17 07:20:58 +0700

    Add files via upload
    Update Premium Intro

--- script.js
+++ script.js
@@ -2,11 +2,40 @@
 
 console.log("Blackjack Rooms Loaded");
 
-// Contoh fungsi tombol
+/* BUTTON EFFECT */
 const buttons = document.querySelectorAll(".btn");
 
 buttons.forEach((btn) => {
+
   btn.addEventListener("click", () => {
-    console.log("Button clicked");
+
+    btn.style.transform = "scale(0.98)";
+
+    setTimeout(() => {
+      btn.style.transform = "";
+    },150);
+
   });
+
 });
+
+/* PREMIUM INTRO */
+window.onload = function(){
+
+    const intro = document.getElementById("intro");
+
+    setTimeout(() => {
+
+        intro.style.opacity = "0";
+
+        setTimeout(() => {
+
+            intro.style.display = "none";
+
+            document.body.style.overflow = "auto";
+
+        },1200);
+
+    },3000);
+
+};
